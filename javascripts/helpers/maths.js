e043c3bc49d90ca919290a4e4e868baf0e3f7e64
Mark Okafor <okaformark@gmail.com>
2019-04-09 21:51:00 -0500

    calculator works

---
+++ javascripts/helpers/maths.js
@@ -0,0 +1,14 @@
+const add = (num1, num2) => {
+    return num1*1 + num2*1;
+};
+const substract = (num1,num2) => {
+    return num1*1 - num2*1;
+};
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+
+export default {add, substract, multiply, divide};
